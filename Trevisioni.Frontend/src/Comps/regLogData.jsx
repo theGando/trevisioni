45bcba3816be67839ffcf1ed6174135faa1fb4c7
Michele Bortot <31648634+EkimoCode@users.noreply.github.com>
2021-06-03 00:10:23 +0200

    fix merge

--- Trevisioni.Frontend/src/Comps/regLogData.jsx
+++ Trevisioni.Frontend/src/Comps/regLogData.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { AppBar, RaisedButton, TextField } from 'material-ui/core';
+import { AppBar, RaisedButton, TextField } from '@material-ui/core';
 
 export class LogData extends Component {
     render() {
