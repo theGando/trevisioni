7a9f4c58349a529718fc66b007c130fe99ffcae5
theGando <tommy.gandin2000@gmail.com>
2021-06-14 23:13:57 +0200

    robe

--- Trevisioni.Frontend/src/Comps/radioButton.jsx
+++ Trevisioni.Frontend/src/Comps/radioButton.jsx
@@ -1,61 +1,32 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
+import * as React from 'react';
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
-const styles = theme => ({
-    root: {
-        display: 'flex',
-    },
-    formControl: {
-        margin: theme.spacing.unit * 3,
-    },
-    group: {
-        margin: `${theme.spacing.unit}px 0`,
-    },
-});
+export default function ControlledRadioButtonsGroup() {
+    const [value, setValue] = React.useState(null);
 
-class RadioButtonsGroup extends React.Component {
-    state = {
-        value: null,
+    const handleChange = (event) => {
+        setValue(event.target.value);
     };
 
-    handleChange = event => {
-        this.setState({ value: event.target.value });
-    };
-
-    render() {
-        const { classes } = this.props;
-
-        return (
-            <div className={classes.root}>
-                <FormControl component="fieldset" className={classes.formControl}>
-                    <FormLabel component="legend">Gender</FormLabel>
-                    <RadioGroup
-                        aria-label="Art Genre"
-                        name="artGenre"
-                        className={classes.group}
-                        value={this.state.value}
-                        onChange={this.handleChange}
-                    >
-                        <FormControlLabel value="music" control={<Radio />} label="Musica" />
-                        <FormControlLabel value="poetry" control={<Radio />} label="Poesia" />
-                        <FormControlLabel value="theatre" control={<Radio />} label="Teatro" />
-                        <FormControlLabel value="expo" control={<Radio />} label="Mostra" />
-                        <FormControlLabel value="stand" control={<Radio />} label="Stand" />
-                        <FormControlLabel value="other" control={<Radio />} label="Altro" />
-                    </RadioGroup>
-                </FormControl>
-            </div>
-        );
-    }
+    return (
+        <FormControl component="fieldset">
+            <FormLabel component="legend">Art Genre</FormLabel>
+            <RadioGroup
+                aria-label="Genre"
+                name="controlled-radio-buttons-group"
+                value={value}
+                onChange={handleChange}
+            >
+                <FormControlLabel value="music" control={<Radio />} label="Music" />
+                <FormControlLabel value="theatre" control={<Radio />} label="Theatre" />
+                <FormControlLabel value="lectures" control={<Radio />} label="Lectures" />
+                <FormControlLabel value="visualArts" control={<Radio />} label="Visual Arts" />
+                <FormControlLabel value="other" control={<Radio />} label="Other" />
+            </RadioGroup>
+        </FormControl>
+    );
 }
-RadioButtonsGroup.propTypes = {
-    classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(RadioButtonsGroup);
