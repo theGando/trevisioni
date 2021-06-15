defeea040a39fa294061ffa405c47d3875e40836
theGando <tommy.gandin2000@gmail.com>
2021-06-15 22:22:54 +0200

    oioioi la tua b vuole solo noi

--- Trevisioni.Frontend/src/Pages/Gallery.jsx
+++ Trevisioni.Frontend/src/Pages/Gallery.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Box, Container, Typography } from "@material-ui/core";
-// import ImageList from '@material-ui/core/ImageList';
-// import ImageListItem from '@material-ui/core/ImageListItem';
+import ImageList from '@material-ui/core/ImageList';
+import ImageListItem from '@material-ui/core/ImageListItem';
 import useStyles from '../Comps/styles';
 
 const itemData = [
     {
         img: '',
+
     },
 ]
 function Gallery() {
@@ -19,7 +20,7 @@ function Gallery() {
                 </Typography>
             </Container>
             <Box sx={{ width: 500, height: 450, overflowY: 'scroll' }}>
-                {/* <ImageList variant="masonry" cols={3} gap={8}>
+                {<ImageList variant="masonry" cols={5} gap={8}>
                     {itemData.map((item) => (
                         <ImageListItem key={item.img}>
                             <img
@@ -29,7 +30,7 @@ function Gallery() {
                             />
                         </ImageListItem>
                     ))}
-                </ImageList> */}
+                </ImageList>}
             </Box>
         </div>
     )
